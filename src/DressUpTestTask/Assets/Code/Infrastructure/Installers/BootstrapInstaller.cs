3f19b18ab317d8725731ffec21c201ecc03e40c4
Zyuzin Alexander <alexander9alex@mail.ru>
2025-03-15 11:34:38 +0500

    Add project context; make game loading

---
+++ src/DressUpTestTask/Assets/Code/Infrastructure/Installers/BootstrapInstaller.cs
@@ -0,0 +1,37 @@
+﻿using Code.Infrastructure.States.Factory;
+using Code.Infrastructure.States.GameStates;
+using Code.Infrastructure.States.StateMachine;
+using Zenject;
+
+namespace Code.Infrastructure.Installers
+{
+  public class BootstrapInstaller : MonoInstaller, IInitializable
+  {
+    public override void InstallBindings()
+    {
+      BindInfrastructureServices();
+      BindGameStateFactory();
+      BindGameStates();
+      BindGameStateMachine();
+    }
+
+    private void BindInfrastructureServices() =>
+      Container.BindInterfacesAndSelfTo<BootstrapInstaller>().FromInstance(this).AsSingle();
+
+    private void BindGameStateFactory() =>
+      Container.Bind<IStateFactory>().To<StateFactory>().AsSingle();
+
+    private void BindGameStates()
+    {
+      Container.BindInterfacesAndSelfTo<BootstrapState>().AsSingle();
+      Container.BindInterfacesAndSelfTo<LoadingGameState>().AsSingle();
+      Container.BindInterfacesAndSelfTo<GameLoopState>().AsSingle();
+    }
+
+    private void BindGameStateMachine() =>
+      Container.Bind<IGameStateMachine>().To<GameStateMachine>().AsSingle();
+
+    public void Initialize() =>
+      Container.Resolve<IGameStateMachine>().Enter<BootstrapState>();
+  }
+}
--- src/DressUpTestTask/Assets/Code/Infrastructure/States/GameStates/GameLoopState.cs
+++ src/DressUpTestTask/Assets/Code/Infrastructure/States/GameStates/GameLoopState.cs
@@ -1,8 +1,13 @@
 using Code.Infrastructure.States.StateInfrastructure;
-  
+using UnityEngine;
+
 namespace Code.Infrastructure.States.GameStates
 {
   public class GameLoopState : EndOfFrameExitState
   {
+    public override void Enter()
+    {
+      Debug.Log("Game started!");
+    }
   }
 }
