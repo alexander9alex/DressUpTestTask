bf74b6411cde2876f43220330bd2fbf792b61e6a
Zyuzin Alexander <alexander9alex@mail.ru>
2025-03-15 11:11:56 +0500

    Implement game state machine; make base game architecture

---
+++ src/DressUpTestTask/Assets/Code/Infrastructure/States/Factory/IStateFactory.cs
@@ -0,0 +1,9 @@
+﻿using Code.Infrastructure.States.StateInfrastructure;
+
+namespace Code.Infrastructure.States.Factory
+{
+  public interface IStateFactory
+  {
+    TState GetState<TState>() where TState : class, IExitableState;
+  }
+}
